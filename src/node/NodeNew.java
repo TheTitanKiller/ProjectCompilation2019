d1a23fd6c69848d55227f2fc4cbfe508d4168d37
mcerutti <marc.cerutti@etu.u-bordeaux.fr>
2019-05-02 20:39:13 +0200

    la plupart des erreurs, sauf les ranges

--- src/node/NodeNew.java
+++ src/node/NodeNew.java
@@ -1,17 +1,21 @@
 package node;
 
+import errors.CustomError;
+import type.TypePointer;
+
 public final class NodeNew extends Node
 {
     public NodeNew(int start, int end, Node stm)
     {
 	super(start, end, stm);
     }
-    
+
     @Override public void checksType()
     {
-	return;
+	if (!(((NodeExp) get(0)).getType() instanceof TypePointer))
+	{ throw new CustomError(getClass().getSimpleName() + ": not a pointer type.", this); }
     }
-    
+
     @Override public NodeNew clone()
     {
 	return new NodeNew(this.start, this.end, (Node) get(0).clone());
