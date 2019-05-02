109d20798e7f0d3e94fa3f5002b19638e71e4aba
mcerutti <marc.cerutti@etu.u-bordeaux.fr>
2019-05-02 16:51:51 +0200

    p = null, trop permissif mainteant

--- src/node/NodeAssign.java
+++ src/node/NodeAssign.java
@@ -2,6 +2,7 @@ package node;
 
 import errors.CustomError;
 import type.Type;
+import type.TypePointer;
 
 public final class NodeAssign extends Node
 {
@@ -9,30 +10,34 @@ public final class NodeAssign extends Node
     {
 	super(start, end, lhs, rhs);
     }
-
+    
     @Override public void checksType()
     {
 	get(0).checksType();
 	get(1).checksType();
 	Type lhsType = getLhs().getType();
 	Type rhsType = getRhs().getType();
-	if (lhsType == null || rhsType == null || !lhsType.equals(rhsType))
-	{ throw new CustomError(getClass().getSimpleName() + ": assignment not of the same type.", this); }
+	if (lhsType == null || rhsType == null)
+	{ throw new CustomError(getClass().getSimpleName() + ": assignment on null type.", this); }
+	if (!lhsType.equals(rhsType) && lhsType instanceof TypePointer && rhsType instanceof TypePointer
+		&& ((TypePointer) rhsType).size() != 0)
+	{ throw new CustomError(getClass().getSimpleName() + ": assignment not on same type.", this); }
+	
     }
-
+    
     @Override public NodeAssign clone()
     {
 	return new NodeAssign(this.start, this.end, (NodeExp) getLhs().clone(), (NodeExp) getRhs().clone());
     };
-
+    
     private NodeExp getLhs()
     {
 	return (NodeExp) get(0);
     }
-
+    
     private NodeExp getRhs()
     {
 	return (NodeExp) get(1);
     };
-
+    
 }
